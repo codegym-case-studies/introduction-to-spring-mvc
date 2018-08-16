a743a4d63ac7883a08b2d4d892aef97987f7ae88
binhsonnguyen <son@binhsonnguyen.com>
2018-08-16 18:44:15 +0700

    use pure java approach to init and mapping a dispatcher servlet

---
+++ src/main/java/showcase/codegym/AppInit.java
@@ -0,0 +1,30 @@
+package showcase.codegym;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class AppInit implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+        WebApplicationContext context = getContext();
+        ContextLoaderListener contextLoaderListener = new ContextLoaderListener(context);
+        servletContext.addListener(contextLoaderListener);
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("frontController", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+
+    private WebApplicationContext getContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+        applicationContext.register(AppConfig.class);
+        return applicationContext;
+    }
+
+}
