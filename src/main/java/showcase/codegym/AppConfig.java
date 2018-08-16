c57643af033a2341b4da2a1edc6fc4dc5c0974f0
binhsonnguyen <son@binhsonnguyen.com>
2018-08-16 18:01:25 +0700

    use annotation based approach to config app's context

---
+++ src/main/java/showcase/codegym/AppConfig.java
@@ -0,0 +1,11 @@
+package showcase.codegym;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "showcase.codegym")
+public class AppConfig {
+}
