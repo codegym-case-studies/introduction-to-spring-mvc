851eb86e351716228e97ef3b91e4c182afa06779
binhsonnguyen <son@binhsonnguyen.com>
2018-08-17 16:46:45 +0700

    use pure java approach to init jsp view resolver

--- src/main/java/showcase/codegym/AppConfig.java
+++ src/main/java/showcase/codegym/AppConfig.java
@@ -1,11 +1,23 @@
 package showcase.codegym;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "showcase.codegym")
 public class AppConfig {
+    @Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/views/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
 }
