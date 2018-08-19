6dce1b053abe5de7d3ed3c45d222d3f70f7ff9b3
binhsonnguyen <son@binhsonnguyen.com>
2018-08-19 11:26:26 +0700

    dashboard page and its controller

--- src/main/java/showcase/codegym/controller/LoginController.java
+++ src/main/java/showcase/codegym/controller/LoginController.java
@@ -2,16 +2,25 @@ package showcase.codegym.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import showcase.codegym.viewmodel.UserCredential;
 
 @Controller
+@RequestMapping("/login")
 public class LoginController {
 
-    @GetMapping("/login")
+    @GetMapping()
     public ModelAndView login() {
         UserCredential emptyCredential = new UserCredential();
         ModelAndView loginModelAndView = new ModelAndView("login/login", "credential", emptyCredential);
+        return loginModelAndView;
+    }
+
+    @PostMapping()
+    public ModelAndView dashboard() {
+        ModelAndView loginModelAndView = new ModelAndView("dashboard/dashboard");
         return loginModelAndView;
     }
 }
---
+++ src/main/webapp/WEB-INF/views/dashboard/dashboard.jsp
@@ -0,0 +1,27 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+<h1>Welcome</h1>
+<table>
+    <tr>
+        <td><b>Account: </b></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td><b>Name: </b></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td><b>Email: </b></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td><b>Age: </b></td>
+        <td></td>
+    </tr>
+</table>
+</body>
+</html>
