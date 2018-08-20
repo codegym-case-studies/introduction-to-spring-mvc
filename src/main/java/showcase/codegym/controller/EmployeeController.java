071599d0b4ac5159be36087e4a48be7e1c94d63e
binhsonnguyen <son@binhsonnguyen.com>
2018-08-20 19:24:46 +0700

    employee submition

--- src/main/java/showcase/codegym/controller/EmployeeController.java
+++ src/main/java/showcase/codegym/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package showcase.codegym.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import showcase.codegym.viewmodel.EmployeeDetails;
@@ -11,9 +13,15 @@ import showcase.codegym.viewmodel.EmployeeDetails;
 public class EmployeeController {
 
     @GetMapping()
-    public ModelAndView employeeCreation() {
+    public ModelAndView employeeSubmition() {
         EmployeeDetails details = new EmployeeDetails();
         ModelAndView modelAndView = new ModelAndView("employee/create", "details", details);
+        return modelAndView;
+    }
+
+    @PostMapping()
+    public ModelAndView employeeCreation(@ModelAttribute("details") EmployeeDetails employeeDetails) {
+        ModelAndView modelAndView = new ModelAndView("employee/info", "profile", employeeDetails);
         return modelAndView;
     }
 }
